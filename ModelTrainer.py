a2eb37589829995c1c303236aa23e9ee159e1db9
j.curtis.webb@gmail.com <j.curtis.webb@gmail.com>
2024-01-10 19:04:43 -0700

    Create first version of LSTM trainer

--- ModelTrainer.py
+++ ModelTrainer.py
@@ -4,7 +4,7 @@ class ModelTrainer(ABC):
     def __init__(self):
         self.schedule = {'M':('16:15:00', 23.5), 'T':('16:15:00', 23.5), 'W':('16:15:00', 23.5), 'Th':('16:15:00', 23.5), 'F':('16:15:00', 23.5)}
         self.pred_type = 'bc'
-        self.model = None
+        self._model = None
         self._data = None
         self._hyperparams = None
          
@@ -20,9 +20,11 @@ class ModelTrainer(ABC):
     def hyperparams(self):
         return self._hyperparams
     
+    
     @hyperparams.setter
     def update_hyperparams(self, **kwargs):
         self._hyperparams = self._hyperparams
+    
     
     @abstractmethod
     def run():
